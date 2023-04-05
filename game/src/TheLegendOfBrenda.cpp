518c1e865af76b47cd21e60cb596d055e1171aa2
Jesus Ramis <jramis@dxc.com>
2023-04-05 13:39:47 +0200

    JRG - Circulo se mueve al hacer click en una posición de la pantalla

--- game/src/TheLegendOfBrenda.cpp
+++ game/src/TheLegendOfBrenda.cpp
@@ -1,38 +1,50 @@
-/*******************************************************************************************
-*
-*   raylib game template
-*
-*   <Game title>
-*   <Game description>
-*
-*   This game has been created using raylib (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-* 
-*   Copyright (c) 2021 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
-#include <iostream>
 
-//GameScreen currentScreen = LOGO;
+int main() {
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
-static const int screenWidth = 800;
-static const int screenHeight = 450;
+    InitWindow(screenWidth, screenHeight, "Mover circulo al hacer clic");
 
-int main(void) {
+    const float circleRadius = 25.0f;
+    Vector2 circlePosition = { (float)screenWidth / 2, (float)screenHeight / 2 };
+    const float circleSpeed = 0.1f;
 
-    std::cout << "Hello World!\n";
-  
-   /* InitWindow(screenWidth, screenHeight, "raylib game template");
-
-    SetTargetFPS(60);
+    bool isMoving = false;
+    Vector2 clickPosition = { 0.0f, 0.0f };
 
     while (!WindowShouldClose()) {
+        // Detectar si se ha hecho clic en el botón izquierdo del ratón
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            // Obtener la posición del clic
+            clickPosition = GetMousePosition();
+
+            // Establecer la variable de movimiento en verdadero
+            isMoving = true;
+        }
+
+        if (isMoving) {
+            // Actualizar la posición del círculo gradualmente hacia la posición del clic
+            if (circlePosition.x < clickPosition.x) circlePosition.x += circleSpeed;
+            else if (circlePosition.x > clickPosition.x) circlePosition.x -= circleSpeed;
+
+            if (circlePosition.y < clickPosition.y) circlePosition.y += circleSpeed;
+            else if (circlePosition.y > clickPosition.y) circlePosition.y -= circleSpeed;
+
+            // Si el círculo está cerca de la posición del clic, detener el movimiento y restablecer la variable de movimiento
+            if (CheckCollisionCircles(circlePosition, circleRadius, clickPosition, 0.1f)) {
+                isMoving = false;
+            }
+        }
+
+        // Dibujar el círculo en su posición actual
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawCircleV(circlePosition, circleRadius, MAROON);
+        EndDrawing();
     }
 
-    CloseWindow();*/
+    CloseWindow();
 
     return 0;
 }
